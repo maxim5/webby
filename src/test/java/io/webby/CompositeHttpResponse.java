27f546aa338073d8b915b643da7d362c783eb6aa
maxim5 <smartmaxim@gmail.com>
2021-08-07 15:49:30 +0200

    extract ContentHolder

--- src/test/java/io/webby/CompositeHttpResponse.java
+++ src/test/java/io/webby/CompositeHttpResponse.java
@@ -4,13 +4,14 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufHolder;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.*;
+import io.webby.netty.response.ContentHolder;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.Queue;
 import java.util.stream.Stream;
 
-public class CompositeHttpResponse extends DefaultHttpResponse implements ByteBufHolder {
+public class CompositeHttpResponse extends DefaultHttpResponse implements ContentHolder {
     private ByteBuf content = Unpooled.EMPTY_BUFFER;
 
     protected CompositeHttpResponse(HttpVersion version, HttpResponseStatus status, HttpHeaders headers) {
@@ -38,61 +39,9 @@ public class CompositeHttpResponse extends DefaultHttpResponse implements ByteBu
     }
 
     @Override
-    public ByteBufHolder copy() {
-        return replace(content.copy());
-    }
-
-    @Override
-    public ByteBufHolder duplicate() {
-        return replace(content.duplicate());
-    }
-
-    @Override
-    public ByteBufHolder retainedDuplicate() {
-        return replace(content.retainedDuplicate());
-    }
-
-    @Override
     public ByteBufHolder replace(ByteBuf content) {
-        return null;
-    }
-
-    @Override
-    public int refCnt() {
-        return content.refCnt();
-    }
-
-    @Override
-    public ByteBufHolder retain() {
-        content.retain();
-        return this;
-    }
-
-    @Override
-    public ByteBufHolder retain(int increment) {
-        content.retain(increment);
-        return this;
-    }
-
-    @Override
-    public ByteBufHolder touch() {
-        content.touch();
-        return this;
-    }
-
-    @Override
-    public ByteBufHolder touch(Object hint) {
-        content.touch(hint);
-        return this;
-    }
-
-    @Override
-    public boolean release() {
-        return content.release();
-    }
-
-    @Override
-    public boolean release(int decrement) {
-        return content.release(decrement);
+        CompositeHttpResponse response = new CompositeHttpResponse(protocolVersion(), status(), headers());
+        response.content = content;
+        return response;
     }
 }
