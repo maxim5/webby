31b0ae0a46b22266dd4d538cea923eabcd3601d1
maxim5 <smartmaxim@gmail.com>
2021-08-19 20:34:39 +0200

    allow to use custom json library from settings

--- src/main/java/io/webby/netty/marshal/MarshallerFactory.java
+++ src/main/java/io/webby/netty/marshal/MarshallerFactory.java
@@ -1,27 +1,56 @@
 package io.webby.netty.marshal;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.flogger.FluentLogger;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import io.webby.app.AppConfigException;
+import io.webby.app.Settings;
 import io.webby.common.InjectorHelper;
 import io.webby.url.annotate.Marshal;
 import io.webby.util.EasyClasspath;
+import io.webby.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Level;
+
 public class MarshallerFactory implements Provider<Json> {
+    private static final FluentLogger log = FluentLogger.forEnclosingClass();
+    private static final ImmutableMap<String, Pair<String, Class<? extends Json>>> SUPPORTED_JSON = ImmutableMap.of(
+        "google:gson", Pair.of("com.google.gson.Gson", GsonMarshaller.class),
+        "fasterxml:jackson-databind", Pair.of("com.fasterxml.jackson.databind.ObjectMapper", JacksonMarshaller.class),
+        "alibaba:fastjson", Pair.of("com.alibaba.fastjson.JSON", FastJsonMarshaller.class)
+    );
+
     private final Class<? extends Json> jsonMarshallerClass;
     @Inject private InjectorHelper helper;
 
-    public MarshallerFactory() {
-        if (EasyClasspath.isInClassPath("com.google.gson.Gson")) {
-            jsonMarshallerClass = GsonMarshaller.class;
-        } else if (EasyClasspath.isInClassPath("com.fasterxml.jackson.databind.ObjectMapper")) {
-            jsonMarshallerClass = JacksonMarshaller.class;
-        } else if (EasyClasspath.isInClassPath("com.alibaba.fastjson.JSON")) {
-            jsonMarshallerClass = FastJsonMarshaller.class;
-        } else {
-            throw new AppConfigException("No JSON library found in classpath. Scanned: Gson, Jackson, Fastjson");
+    @Inject
+    public MarshallerFactory(@NotNull Settings settings) {
+        jsonMarshallerClass = pickJsonMarshaller(settings);
+    }
+
+    @NotNull
+    private static Class<? extends Json> pickJsonMarshaller(@NotNull Settings settings) {
+        String property = settings.getProperty("json.library");
+        Pair<String, Class<? extends Json>> pair = SUPPORTED_JSON.get(property);
+        if (property != null && pair == null) {
+            log.at(Level.CONFIG).log("Unrecognized json library. Available: %s".formatted(SUPPORTED_JSON.keySet()));
         }
+        if (pair != null) {
+            if (!EasyClasspath.isInClassPath(pair.first())) {
+                log.at(Level.WARNING).log("Json library not found in classpath: %s".formatted(pair.first()));
+            }
+            return pair.second();
+        }
+
+        for (Pair<String, Class<? extends Json>> val : SUPPORTED_JSON.values()) {
+            if (EasyClasspath.isInClassPath(val.first())) {
+                log.at(Level.INFO).log("Using Json library: %s".formatted(val.first()));
+                return val.second();
+            }
+        }
+        throw new AppConfigException("No JSON library found in classpath. Scanned: %s".formatted(SUPPORTED_JSON.keySet()));
     }
 
     public @NotNull Marshaller getMarshaller(@NotNull Marshal marshal) {
