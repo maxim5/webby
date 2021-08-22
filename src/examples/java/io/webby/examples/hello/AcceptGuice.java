4e3a63a3a7540cc8e1ac79dbd4deedc0ef36421a
maxim5 <smartmaxim@gmail.com>
2021-08-22 14:39:08 +0200

    add integration tests

---
+++ src/examples/java/io/webby/examples/hello/AcceptGuice.java
@@ -0,0 +1,52 @@
+package io.webby.examples.hello;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.webby.app.Settings;
+import io.webby.netty.response.HttpResponseFactory;
+import io.webby.url.annotate.GET;
+import io.webby.url.annotate.Json;
+import io.webby.url.annotate.POST;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
+public class AcceptGuice {
+    @GET(url="/guice/simple")
+    public @NotNull Charset simple(@NotNull Settings settings) {
+        return settings.charset();
+    }
+
+    @GET(url="/guice/simple/2")
+    public @NotNull FullHttpResponse simple_two(@NotNull Settings settings, @NotNull HttpResponseFactory factory) {
+        return factory.newResponse(settings.charset().toString(), HttpResponseStatus.OK);
+    }
+
+    @GET(url="/guice/param/str/{id}")
+    public @NotNull String str_param_settings(@NotNull String id, @NotNull Settings settings) {
+        return "%s:%s".formatted(id, settings.charset());
+    }
+
+    @GET(url="/guice/param/int/{id}")
+    public @NotNull String int_param_settings(int id, @NotNull Settings settings) {
+        return "%d:%s".formatted(id, settings.charset());
+    }
+
+    @GET(url="/guice/settings_request")
+    public @NotNull Charset settings_request(@NotNull Settings settings, @NotNull HttpRequest request) {
+        return settings.charset();
+    }
+
+    @GET(url="/guice/request_settings")
+    public @NotNull Charset request_settings(@NotNull HttpRequest request, @NotNull Settings settings) {
+        return settings.charset();
+    }
+
+    @POST(url="/guice/request_settings_content")
+    public @NotNull String request_settings_content(@NotNull HttpRequest request, @NotNull Settings settings,
+                                                    @Json Map<String, Object> content) {
+        return "%s:%s".formatted(content, settings.charset());
+    }
+}
