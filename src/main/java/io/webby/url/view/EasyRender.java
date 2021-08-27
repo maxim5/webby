19f8727e67a476874f510ef889cfc99cfed394be
maxim5 <smartmaxim@gmail.com>
2021-08-28 00:21:36 +0200

    minor

--- src/main/java/io/webby/url/view/EasyRender.java
+++ src/main/java/io/webby/url/view/EasyRender.java
@@ -4,6 +4,7 @@ import io.webby.common.SystemProperties;
 import io.webby.util.EasyCast;
 import io.webby.util.func.ThrowConsumer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.util.Map;
@@ -12,23 +13,25 @@ import java.util.function.Function;
 import static io.webby.util.EasyIO.Close.closeQuietly;
 
 public class EasyRender {
-    @NotNull
-    public static <E extends Throwable> String writeToString(ThrowConsumer<Writer, E> consumer) throws E {
+    public static <E extends Throwable>
+            @NotNull String writeToString(@NotNull ThrowConsumer<Writer, E> consumer) throws E {
         return writeToString(consumer, SystemProperties.DEFAULT_SIZE_CHARS);
     }
 
-    @NotNull
-    public static <E extends Throwable> String writeToString(ThrowConsumer<Writer, E> consumer, int size) throws E {
+    public static <E extends Throwable>
+            @NotNull String writeToString(@NotNull ThrowConsumer<Writer, E> consumer, int size) throws E {
         Writer writer = new StringWriter(size);
         consumer.accept(writer);
         return writer.toString();
     }
 
-    public static <E extends Throwable> byte[] writeToBytes(ThrowConsumer<Writer, E> consumer) throws E {
+    public static <E extends Throwable>
+            byte @NotNull [] writeToBytes(@NotNull ThrowConsumer<Writer, E> consumer) throws E {
         return writeToBytes(consumer, SystemProperties.DEFAULT_SIZE_BYTES);
     }
 
-    public static <E extends Throwable> byte[] writeToBytes(ThrowConsumer<Writer, E> consumer, int size) throws E {
+    public static <E extends Throwable>
+            byte @NotNull [] writeToBytes(@NotNull ThrowConsumer<Writer, E> consumer, int size) throws E {
         ByteArrayOutputStream output = new ByteArrayOutputStream(size);
         Writer writer = new OutputStreamWriter(output);
         consumer.accept(writer);
@@ -36,17 +39,20 @@ public class EasyRender {
         return output.toByteArray();
     }
 
-    public static <E extends Throwable> byte[] outputToBytes(ThrowConsumer<OutputStream, E> consumer) throws E {
+    public static <E extends Throwable>
+            byte @NotNull [] outputToBytes(@NotNull ThrowConsumer<OutputStream, E> consumer) throws E {
         return outputToBytes(consumer, SystemProperties.DEFAULT_SIZE_CHARS);
     }
 
-    public static <E extends Throwable> byte[] outputToBytes(ThrowConsumer<OutputStream, E> consumer, int size) throws E {
+    public static <E extends Throwable>
+            byte @NotNull [] outputToBytes(@NotNull ThrowConsumer<OutputStream, E> consumer, int size) throws E {
         ByteArrayOutputStream output = new ByteArrayOutputStream(size);
         consumer.accept(output);
         return output.toByteArray();
     }
 
-    public static <T, K, V, E extends Throwable> Map<K, V> castMapOrFail(T object, Function<T, E> error) throws E {
+    public static <T, K, V, E extends Throwable>
+            @NotNull Map<K, V> castMapOrFail(@Nullable T object, @NotNull Function<T, E> error) throws E {
         if (object instanceof Map<?, ?> map) {
             return EasyCast.castMap(map);
         }
