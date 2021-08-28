6eaa8c8ab51cabbe7e21ab36a744ddb6ca5ce0fc
maxim5 <smartmaxim@gmail.com>
2021-08-28 17:10:19 +0200

    minor

--- src/examples/java/io/webby/examples/hello/AcceptContent.java
+++ src/examples/java/io/webby/examples/hello/AcceptContent.java
@@ -12,12 +12,12 @@ public class AcceptContent {
     @Param(var="id") public static final IntConverter paramId = IntConverter.POSITIVE;
 
     @POST(url="/int/{id}")
-    public Object update_integer(int id) {
+    public Object no_content(int id) {
         return new HashMap<>();
     }
 
     @POST(url="/intstr/{str}/{y}")
-    public String update_str_int(CharSequence str, int y, Object content) {
+    public String content_object(CharSequence str, int y, Object content) {
         return "Vars: str=%s y=%d content=<%s>".formatted(str, y, content.toString());
     }
 }
