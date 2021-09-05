d96166b58d4aed685c69394a742ba13ee76a91ab
maxim5 <smartmaxim@gmail.com>
2021-09-05 12:56:54 +0200

    minor

--- src/main/java/io/webby/db/kv/impl/BaseKeyValueFactory.java
+++ src/main/java/io/webby/db/kv/impl/BaseKeyValueFactory.java
@@ -1,7 +1,6 @@
 package io.webby.db.kv.impl;
 
 import com.google.inject.Inject;
-import io.webby.app.AppConfigException;
 import io.webby.app.Settings;
 import io.webby.common.Lifetime;
 import io.webby.db.kv.KeyValueDb;
@@ -14,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import static io.webby.app.AppConfigException.*;
+import static io.webby.app.AppConfigException.assure;
 import static io.webby.util.EasyCast.castAny;
 
 public abstract class BaseKeyValueFactory implements InternalKeyValueFactory, Closeable {
