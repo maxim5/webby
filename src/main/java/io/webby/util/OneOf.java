e6b780b652b490b9c9f62fcae517ffb69627d2bc
maxim5 <smartmaxim@gmail.com>
2021-10-23 21:24:44 +0200

    minor: one-of util

---
+++ src/main/java/io/webby/util/OneOf.java
@@ -0,0 +1,62 @@
+package io.webby.util;
+
+public class OneOf<U, V> {
+    private final U first;
+    private final V second;
+    private final Which which;
+
+    private OneOf(U first, V second, Which which) {
+        this.first = first;
+        this.second = second;
+        this.which = which;
+    }
+
+    public static <U, V> OneOf<U, V> of(U first, V second) {
+        assert first == null ^ second == null : "Exactly one must be non-null: first=%s second=%s".formatted(first, second);
+        if (first != null) {
+            return ofFirst(first);
+        } else {
+            return ofSecond(second);
+        }
+    }
+
+    public static <U, V> OneOf<U, V> ofFirst(U first) {
+        assert first != null : "Expected non-null `first`";
+        return new OneOf<>(first, null, Which.FIRST);
+    }
+
+    public static <U, V> OneOf<U, V> ofSecond(V second) {
+        assert second != null : "Expected non-null `second`";
+        return new OneOf<>(null, second, Which.SECOND);
+    }
+
+    public Which getCase() {
+        return which;
+    }
+
+    public boolean hasFirst() {
+        return which == Which.FIRST;
+    }
+
+    public U first() {
+        return first;
+    }
+
+    public boolean hasSecond() {
+        return which == Which.SECOND;
+    }
+
+    public V second() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return hasFirst() ? "OneOf{first=%s}".formatted(first) : "OneOf{second=%s}".formatted(second);
+    }
+
+    public enum Which {
+        FIRST,
+        SECOND
+    }
+}
