ea3df606562af5787d57565ffa30bbff9b5eb7da
maxim5 <smartmaxim@gmail.com>
2021-10-31 07:21:31 +0100

    Instant -> Timestamp

--- src/main/java/io/webby/util/sql/adapter/InstantJdbcAdapter.java
+++ src/main/java/io/webby/util/sql/adapter/InstantJdbcAdapter.java
@@ -1,13 +1,14 @@
 package io.webby.util.sql.adapter;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 
 public class InstantJdbcAdapter {
-    public static Instant createInstance(long value) {
-        return Instant.ofEpochSecond(value);
+    public static Instant createInstance(Timestamp value) {
+        return value.toInstant();
     }
 
     public static void fillArrayValues(Instant instance, Object[] array, int start) {
-        array[start] = instance.getEpochSecond();
+        array[start] = Timestamp.from(instance);
     }
 }
