2100820dbe5c918b139f772e0d71d657e9908aa0
maxim5 <smartmaxim@gmail.com>
2021-11-05 10:24:09 +0100

    add another level of indirection

--- src/examplesModel/java/io/webby/examples/model/InnerDataModel.java
+++ src/examplesModel/java/io/webby/examples/model/InnerDataModel.java
@@ -1,6 +1,7 @@
 package io.webby.examples.model;
 
-public record InnerDataModel(long id, Simple simple) {
+public record InnerDataModel(long id, Simple simple, Level1 level1) {
 
     static record Simple(int id, long a, String b) {}
+    static record Level1(int id, Simple simple) {}
 }
