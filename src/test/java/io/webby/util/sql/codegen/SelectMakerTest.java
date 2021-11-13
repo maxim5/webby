c90a0c83ba9b95cd9c6a37df1e737afa07e96d5a
maxim5 <smartmaxim@gmail.com>
2021-11-13 14:47:48 +0100

    fix tests

--- src/test/java/io/webby/util/sql/codegen/SelectMakerTest.java
+++ src/test/java/io/webby/util/sql/codegen/SelectMakerTest.java
@@ -25,9 +25,13 @@ public class SelectMakerTest {
         SelectMaker selectMaker = new SelectMaker(schemaMap.get(Song.class));
 
         assertMaker(selectMaker,
-                    "SELECT author_id FROM song",
                     """
-                    SELECT user.user_id, user.name FROM song
+                    SELECT author_id
+                    FROM song
+                    """,
+                    """
+                    SELECT user.user_id, user.name
+                    FROM song
                     LEFT JOIN user ON song.author_id = user.user_id
                     """);
     }
@@ -42,13 +46,18 @@ public class SelectMakerTest {
         SelectMaker selectMaker = new SelectMaker(schemaMap.get(Single.class));
 
         assertMaker(selectMaker,
-                    "SELECT hit_song_id FROM single",
                     """
-                    SELECT song.song_id, song.author_id FROM single
+                    SELECT hit_song_id
+                    FROM single
+                    """,
+                    """
+                    SELECT song.song_id, song.author_id
+                    FROM single
                     LEFT JOIN song ON single.hit_song_id = song.song_id
                     """,
                     """
-                    SELECT song.song_id, user.user_id, user.name FROM single
+                    SELECT song.song_id, user.user_id, user.name
+                    FROM single
                     LEFT JOIN song ON single.hit_song_id = song.song_id
                     LEFT JOIN user ON song.author_id = user.user_id
                     """);
