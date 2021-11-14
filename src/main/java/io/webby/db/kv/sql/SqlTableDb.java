512889b0c06f2f46b05f6c34059b93dc7f719fcd
maxim5 <smartmaxim@gmail.com>
2021-11-15 00:34:05 +0100

    introduce SqlTableDb

---
+++ src/main/java/io/webby/db/kv/sql/SqlTableDb.java
@@ -0,0 +1,86 @@
+package io.webby.db.kv.sql;
+
+import io.webby.db.kv.KeyValueDb;
+import io.webby.util.sql.api.TableObj;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SqlTableDb<K, V> implements KeyValueDb<K, V> {
+    private final TableObj<K, V> table;
+
+    public SqlTableDb(@NotNull TableObj<K, V> table) {
+        this.table = table;
+    }
+
+    @Override
+    public int size() {
+        return table.count();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return table.isEmpty();
+    }
+
+    @Override
+    public @Nullable V get(@NotNull K key) {
+        return table.getByPkOrNull(key);
+    }
+
+    @Override
+    public boolean containsValue(@NotNull V value) {
+        return value.equals(get(table.keyOf(value)));
+    }
+
+    @Override
+    public @NotNull Iterable<K> keys() {
+        return table.fetchAll().stream().map(table::keyOf).toList();
+    }
+
+    @Override
+    public @NotNull Set<K> keySet() {
+        return table.fetchAll().stream().map(table::keyOf).collect(Collectors.toSet());
+    }
+
+    @Override
+    public @NotNull Collection<V> values() {
+        return table.fetchAll();
+    }
+
+    @Override
+    public @NotNull Set<Map.Entry<K, V>> entrySet() {
+        return table.fetchAll().stream()
+                .map(entity -> new AbstractMap.SimpleEntry<>(table.keyOf(entity), entity))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public void set(@NotNull K key, @NotNull V value) {
+        assert key.equals(table.keyOf(value)) : "Inconsistent key=`%s` and value=`%s`".formatted(key, value);
+        table.updateByPk(value);
+    }
+
+    @Override
+    public void delete(@NotNull K key) {
+        table.deleteByPk(key);
+    }
+
+    @Override
+    public void clear() {
+        for (K key : keys()) {
+            table.deleteByPk(key);
+        }
+    }
+
+    @Override
+    public void flush() {}
+
+    @Override
+    public void close() {}
+}
