691c94ec4cb40ec536a09a0a9ab5d531a12a91c7
maxim5 <smartmaxim@gmail.com>
2021-11-20 18:54:40 +0100

    improve QueryRunner

--- src/main/java/io/webby/util/sql/api/QueryException.java
+++ src/main/java/io/webby/util/sql/api/QueryException.java
@@ -3,18 +3,24 @@ package io.webby.util.sql.api;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class QueryException extends RuntimeException {
     private final String query;
     private final List<Object> args;
 
+    public QueryException(@NotNull String message, @NotNull String query, @Nullable Object @NotNull ... args) {
+        this(message, query, Arrays.asList(args), null);
+    }
+
     public QueryException(@NotNull String message, @NotNull String query, @Nullable Throwable cause) {
         this(message, query, List.of(), cause);
     }
 
-    public QueryException(@NotNull String message, @NotNull String query, Object arg, @Nullable Throwable cause) {
-        this(message, query, List.of(arg), cause);
+    public QueryException(@NotNull String message, @NotNull String query, @Nullable Object arg, @Nullable Throwable cause) {
+        this(message, query, Collections.singletonList(arg), cause);
     }
 
     public QueryException(@NotNull String message, @NotNull String query, @NotNull List<Object> args, @Nullable Throwable cause) {
@@ -31,7 +37,7 @@ public class QueryException extends RuntimeException {
         return args;
     }
 
-    private static @NotNull String format(String message, String query, List<Object> args) {
-        return "%s. Query: %s. Args: %s".formatted(message, query, args);
+    private static @NotNull String format(@NotNull String message, @NotNull String query, @NotNull List<Object> args) {
+        return "%s. Query: `%s`. Args: `%s`".formatted(message, query, args);
     }
 }
--- src/main/java/io/webby/util/sql/api/QueryRunner.java
+++ src/main/java/io/webby/util/sql/api/QueryRunner.java
@@ -1,5 +1,6 @@
 package io.webby.util.sql.api;
 
+import com.google.errorprone.annotations.MustBeClosed;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,29 +15,34 @@ public class QueryRunner {
 
     // Query
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql) throws SQLException {
         Statement statement = connection.createStatement();
         return statement.executeQuery(sql);
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql, @Nullable Object param) throws SQLException {
         PreparedStatement prepared = connection.prepareStatement(sql);
         prepared.setObject(1, param);
         return prepared.executeQuery();
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql, int param) throws SQLException {
         PreparedStatement prepared = connection.prepareStatement(sql);
         prepared.setInt(1, param);
         return prepared.executeQuery();
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql, long param) throws SQLException {
         PreparedStatement prepared = connection.prepareStatement(sql);
         prepared.setLong(1, param);
         return prepared.executeQuery();
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql,
                                        @Nullable Object param1,
                                        @Nullable Object param2) throws SQLException {
@@ -46,6 +52,7 @@ public class QueryRunner {
         return prepared.executeQuery();
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql,
                                        @NotNull Object param1,
                                        @NotNull Object param2,
@@ -57,6 +64,7 @@ public class QueryRunner {
         return prepared.executeQuery();
     }
 
+    @MustBeClosed
     public @NotNull ResultSet runQuery(@NotNull String sql, @Nullable Object @NotNull ... params) throws SQLException {
         PreparedStatement prepared = connection.prepareStatement(sql);
         for (int i = 0; i < params.length; i++) {
@@ -68,51 +76,80 @@ public class QueryRunner {
     // Updates
 
     public int runUpdate(@NotNull String sql) throws SQLException {
-        Statement statement = connection.createStatement();
-        return statement.executeUpdate(sql);
+        try (Statement statement = connection.createStatement()) {
+            return statement.executeUpdate(sql);
+        }
     }
 
     public int runUpdate(@NotNull String sql, @Nullable Object param) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        prepared.setObject(1, param);
-        return prepared.executeUpdate();
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            prepared.setObject(1, param);
+            return prepared.executeUpdate();
+        }
     }
 
     public int runUpdate(@NotNull String sql, int param) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        prepared.setInt(1, param);
-        return prepared.executeUpdate();
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            prepared.setInt(1, param);
+            return prepared.executeUpdate();
+        }
     }
 
     public int runUpdate(@NotNull String sql, long param) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        prepared.setLong(1, param);
-        return prepared.executeUpdate();
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            prepared.setLong(1, param);
+            return prepared.executeUpdate();
+        }
     }
 
     public int runUpdate(@NotNull String sql, @Nullable Object param1, @Nullable Object param2) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        prepared.setObject(1, param1);
-        prepared.setObject(2, param2);
-        return prepared.executeUpdate();
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            prepared.setObject(1, param1);
+            prepared.setObject(2, param2);
+            return prepared.executeUpdate();
+        }
     }
 
     public int runUpdate(@NotNull String sql,
                          @Nullable Object param1,
                          @Nullable Object param2,
                          @Nullable Object param3) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        prepared.setObject(1, param1);
-        prepared.setObject(2, param2);
-        prepared.setObject(3, param3);
-        return prepared.executeUpdate();
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            prepared.setObject(1, param1);
+            prepared.setObject(2, param2);
+            prepared.setObject(3, param3);
+            return prepared.executeUpdate();
+        }
     }
 
     public int runUpdate(@NotNull String sql, @Nullable Object @NotNull ... params) throws SQLException {
-        PreparedStatement prepared = connection.prepareStatement(sql);
-        for (int i = 0; i < params.length; i++) {
-            prepared.setObject(i + 1, params[i]);
+        try (PreparedStatement prepared = connection.prepareStatement(sql)) {
+            for (int i = 0; i < params.length; i++) {
+                prepared.setObject(i + 1, params[i]);
+            }
+            return prepared.executeUpdate();
         }
-        return prepared.executeUpdate();
+    }
+
+    public @NotNull AutoIncResult runAutoIncUpdate(@NotNull String sql,
+                                                   @Nullable Object @NotNull ... params) throws SQLException {
+        try (PreparedStatement prepared = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            for (int i = 0; i < params.length; i++) {
+                prepared.setObject(i + 1, params[i]);
+            }
+
+            // See https://stackoverflow.com/questions/1915166/how-to-get-the-insert-id-in-jdbc
+            int changedRowCount = prepared.executeUpdate();
+            try (ResultSet generatedKeys = prepared.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return new AutoIncResult(changedRowCount, generatedKeys.getLong(1));
+                } else {
+                    throw new QueryException("No generated keys returned. Changed row count=%d".formatted(changedRowCount), sql, params);
+                }
+            }
+        }
+    }
+
+    public record AutoIncResult(int changedRowCount, long lastId) {
     }
 }
