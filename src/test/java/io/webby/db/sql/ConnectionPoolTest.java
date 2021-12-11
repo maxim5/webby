cb4051a8195b60d797e1d3d1a70de6df9cd00561
maxim5 <smartmaxim@gmail.com>
2021-12-11 23:46:53 +0100

    fix the test

--- src/test/java/io/webby/db/sql/ConnectionPoolTest.java
+++ src/test/java/io/webby/db/sql/ConnectionPoolTest.java
@@ -9,16 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ConnectionPoolTest {
     @Test
     public void parseUrl_valid() {
+        assertEquals(Engine.Derby, parseUrl("jdbc:derby:target/tmp/derby/hpjp;databaseName=foo;create=true"));
         assertEquals(Engine.H2, parseUrl("jdbc:h2:file:/path/to/file.h2"));
         assertEquals(Engine.H2, parseUrl("jdbc:h2:mem:foo"));
+        assertEquals(Engine.HyperSQL, parseUrl("jdbc:hsqldb:mem:foo"));
         assertEquals(Engine.MsSqlServer, parseUrl("jdbc:sqlserver://localhost;instance=SQLEXPRESS;databaseName=foo"));
         assertEquals(Engine.MySQL, parseUrl("jdbc:mysql://localhost/foo"));
         assertEquals(Engine.Oracle, parseUrl("jdbc:oracle:thin:@localhost:1521:orclpdb1"));
         assertEquals(Engine.PostgreSQL, parseUrl("jdbc:postgresql://localhost/foo"));
         assertEquals(Engine.SQLite, parseUrl("jdbc:sqlite::memory:"));
-
-        assertEquals(Engine.Unknown, parseUrl("jdbc:hsqldb:mem:foo"));
-        assertEquals(Engine.Unknown, parseUrl("jdbc:derby:target/tmp/derby/hpjp;databaseName=foo;create=true"));
     }
 
     @Test
