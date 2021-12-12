5636e909b3ed19fa27cdcef7d570ad1872085795
maxim5 <smartmaxim@gmail.com>
2021-12-12 22:53:30 +0100

    SQL functions

--- src/main/java/io/webby/orm/api/query/Func.java
+++ src/main/java/io/webby/orm/api/query/Func.java
@@ -1,11 +1,54 @@
 package io.webby.orm.api.query;
 
+import com.google.mu.util.stream.BiStream;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+import static io.webby.orm.api.query.TermType.*;
+
 public enum Func implements Representable {
-    HEX("hex", List.of(TermType.STRING), TermType.STRING);
+    COUNT("count", List.of(WILDCARD), NUMBER),
+    SUM("sum", List.of(NUMBER), NUMBER),
+    AVG("avg", List.of(NUMBER), NUMBER),
+    MAX("max", List.of(NUMBER), NUMBER),
+    MIN("min", List.of(NUMBER), NUMBER),
+    ABS("abs", List.of(NUMBER), NUMBER),
+    SQUARE("square", List.of(NUMBER), NUMBER),
+
+    FIRST("first", List.of(WILDCARD), WILDCARD),
+    FIRST_NUM("first", List.of(NUMBER), NUMBER),
+    FIRST_STR("first", List.of(STRING), STRING),
+
+    LAST("last", List.of(WILDCARD), WILDCARD),
+    LAST_NUM("last", List.of(NUMBER), NUMBER),
+    LAST_STR("last", List.of(STRING), STRING),
+
+    LOWER("lower", List.of(STRING), STRING),
+    LCASE("lcase", List.of(STRING), STRING),
+    UPPER("upper", List.of(STRING), STRING),
+    UCASE("ucase", List.of(STRING), STRING),
+
+    TRIM("trim", List.of(STRING), STRING),
+    LTRIM("ltrim", List.of(STRING), STRING),
+    RTRIM("rtrim", List.of(STRING), STRING),
+
+    ASCII("ascii", List.of(STRING), NUMBER),
+    CHAR("char", List.of(NUMBER), STRING),
+    STR("str", List.of(NUMBER), STRING),
+    LEN("len", List.of(STRING), NUMBER),
+    HEX("hex", List.of(STRING), STRING),
+    SUBSTRING("substring", List.of(STRING, NUMBER, NUMBER), STRING),
+
+    CONCAT("concat", List.of(STRING, STRING), STRING),
+    CONCAT3("concat", List.of(STRING, STRING, STRING), STRING),
+    CONCAT4("concat", List.of(STRING, STRING, STRING, STRING), STRING),
+    CONCAT5("concat", List.of(STRING, STRING, STRING, STRING, STRING), STRING),
+
+    CONCAT_WS("concat_ws", List.of(STRING, STRING, STRING), STRING),
+    CONCAT_WS3("concat_ws", List.of(STRING, STRING, STRING, STRING), STRING),
+    CONCAT_WS4("concat_ws", List.of(STRING, STRING, STRING, STRING, STRING), STRING),
+    CONCAT_WS5("concat_ws", List.of(STRING, STRING, STRING, STRING, STRING, STRING), STRING);
 
     private final String repr;
     private final List<TermType> inputTypes;
@@ -27,11 +70,22 @@ public enum Func implements Representable {
     }
 
     public boolean matchesInput(@NotNull Term term) {
-        return arity() == 1 && TermType.match(inputTypes.get(0), term.type());
+        return arity() == 1 && match(inputTypes.get(0), term.type());
     }
 
     public boolean matchesInput(@NotNull Term term1, @NotNull Term term2) {
-        return arity() == 2 && TermType.match(inputTypes.get(0), term1.type()) && TermType.match(inputTypes.get(1), term2.type());
+        return arity() == 2 && match(inputTypes.get(0), term1.type()) && match(inputTypes.get(1), term2.type());
+    }
+
+    public boolean matchesInput(@NotNull Term term1, @NotNull Term term2, @NotNull Term term3) {
+        return arity() == 3 &&
+               match(inputTypes.get(0), term1.type()) &&
+               match(inputTypes.get(1), term2.type()) &&
+               match(inputTypes.get(2), term3.type());
+    }
+
+    public boolean matchesInput(@NotNull List<Term> terms) {
+        return BiStream.zip(inputTypes, terms).allMatch(((inputType, term) -> match(inputType, term.type())));
     }
 
     public @NotNull TermType resultType() {
@@ -44,5 +98,17 @@ public enum Func implements Representable {
 
     public @NotNull FuncExpr of(@NotNull Term term1, @NotNull Term term2) {
         return new FuncExpr(this, term1, term2);
+    }
+
+    public @NotNull FuncExpr of(@NotNull Term term1, @NotNull Term term2, @NotNull Term term3) {
+        return new FuncExpr(this, term1, term2, term3);
+    }
+
+    public @NotNull FuncExpr of(@NotNull Term ... terms) {
+        return of(List.of(terms));
+    }
+
+    public @NotNull FuncExpr of(@NotNull List<Term> terms) {
+        return new FuncExpr(this, terms);
     }
 }
--- src/main/java/io/webby/orm/api/query/FuncExpr.java
+++ src/main/java/io/webby/orm/api/query/FuncExpr.java
@@ -2,6 +2,9 @@ package io.webby.orm.api.query;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.webby.orm.api.query.InvalidQueryException.assure;
 
 public class FuncExpr extends Unit implements Term {
@@ -16,7 +19,22 @@ public class FuncExpr extends Unit implements Term {
     public FuncExpr(@NotNull Func func, @NotNull Term term1, @NotNull Term term2) {
         super("%s(%s, %s)".formatted(func.repr(), term1.repr(), term2.repr()), flattenArgsOf(term1, term2));
         this.func = func;
-        assure(func.matchesInput(term1, term2), "Incompatible function `%s` input: [`%s`, `%s`]", func, term1, term2);
+        assure(func.matchesInput(term1, term2), "Incompatible function `%s` inputs: [`%s`, `%s`]", func, term1, term2);
+    }
+
+    public FuncExpr(@NotNull Func func, @NotNull Term term1, @NotNull Term term2, @NotNull Term term3) {
+        super("%s(%s, %s, %s)".formatted(func.repr(), term1.repr(), term2.repr(), term3.repr()),
+              flattenArgsOf(List.of(term1, term2, term3)));
+        this.func = func;
+        assure(func.matchesInput(term1, term2, term3),
+               "Incompatible function `%s` inputs: [`%s`, `%s`, `%s`]", func, term1, term2, term3);
+    }
+
+    public FuncExpr(@NotNull Func func, @NotNull List<Term> terms) {
+        super(func.repr() + terms.stream().map(Representable::repr).collect(Collectors.joining(", ", "(", ")")),
+              flattenArgsOf(terms));
+        this.func = func;
+        assure(func.matchesInput(terms), "Incompatible function `%s` inputs: `%s`", func, terms);
     }
 
     @Override
