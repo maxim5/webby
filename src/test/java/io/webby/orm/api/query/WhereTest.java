b66a6e7f67d846d240d0bf8cf34352a77947f848
maxim5 <smartmaxim@gmail.com>
2021-12-12 23:11:06 +0100

    minor

--- src/test/java/io/webby/orm/api/query/WhereTest.java
+++ src/test/java/io/webby/orm/api/query/WhereTest.java
@@ -1,6 +1,5 @@
 package io.webby.orm.api.query;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static io.webby.orm.api.query.CompareType.GE;
@@ -9,33 +8,17 @@ import static io.webby.orm.api.query.Shortcuts.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WhereTest {
-    private static final Column INT_COLUMN = newColumn("int", TermType.NUMBER);
-    private static final Column STR_COLUMN = newColumn("str", TermType.STRING);
-
     @Test
     public void compare_expr() {
-        assertEquals("WHERE int >= 0", Where.and(GE.compare(INT_COLUMN, num(0))).repr());
-        assertEquals("WHERE int >= 0 AND int <= 5", Where.and(GE.compare(INT_COLUMN, num(0)),
-                                                              LE.compare(INT_COLUMN, num(5))).repr());
+        assertEquals("WHERE i >= 0", Where.and(GE.compare(FakeColumn.INT, num(0))).repr());
+        assertEquals("WHERE i >= 0 AND i <= 5", Where.and(GE.compare(FakeColumn.INT, num(0)),
+                                                          LE.compare(FakeColumn.INT, num(5))).repr());
     }
 
     @Test
     public void like_expr() {
-        assertEquals("WHERE str LIKE str", Where.of(like(STR_COLUMN, STR_COLUMN)).repr());
-        assertEquals("WHERE str LIKE '%'", Where.of(like(STR_COLUMN, literal("%"))).repr());
-        assertEquals("WHERE hex(str) LIKE 'foo%'", Where.of(like(Func.HEX.apply(STR_COLUMN), literal("foo%"))).repr());
-    }
-
-    private static @NotNull Column newColumn(@NotNull String name, @NotNull TermType type) {
-        return new Column() {
-            @Override
-            public @NotNull String name() {
-                return name;
-            }
-            @Override
-            public @NotNull TermType type() {
-                return type;
-            }
-        };
+        assertEquals("WHERE s LIKE foo", Where.of(like(FakeColumn.STR, FakeColumn.FOO)).repr());
+        assertEquals("WHERE s LIKE '%'", Where.of(like(FakeColumn.STR, literal("%"))).repr());
+        assertEquals("WHERE hex(s) LIKE 'foo%'", Where.of(like(Func.HEX.apply(FakeColumn.STR), literal("foo%"))).repr());
     }
 }
