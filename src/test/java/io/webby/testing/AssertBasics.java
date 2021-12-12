f8f943786ed1d06cf97a8d8c93624b09ef00f8f5
maxim5 <smartmaxim@gmail.com>
2021-12-12 12:16:05 +0100

    testing utils

--- src/test/java/io/webby/testing/AssertBasics.java
+++ src/test/java/io/webby/testing/AssertBasics.java
@@ -4,8 +4,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertBasics {
     @SuppressWarnings("unchecked")
@@ -15,5 +17,18 @@ public class AssertBasics {
 
     public static <T> void assertOneOf(@Nullable T value, @NotNull Iterable<T> expected) {
         assertThat(value).isIn(expected);
+    }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static <T> void assertEmpty(@Nullable Optional<T> optional) {
+        assertNotNull(optional);
+        assertTrue(optional.isEmpty());
+    }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static <T> void assertPresent(@Nullable Optional<T> optional, @NotNull T expected) {
+        assertNotNull(optional);
+        assertTrue(optional.isPresent());
+        assertEquals(expected, optional.get());
     }
 }
