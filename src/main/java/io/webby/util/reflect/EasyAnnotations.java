dd27973a0bbde4d6840574fa09e8d7c48d398719
maxim5 <smartmaxim@gmail.com>
2021-12-17 16:04:05 +0100

    minor API fix

--- src/main/java/io/webby/util/reflect/EasyAnnotations.java
+++ src/main/java/io/webby/util/reflect/EasyAnnotations.java
@@ -4,16 +4,17 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
 import java.util.Optional;
 
 public class EasyAnnotations {
-    public static <A extends Annotation> @Nullable A getAnnotationOrNull(@NotNull Class<?> klass,
+    public static <A extends Annotation> @Nullable A getAnnotationOrNull(@NotNull AnnotatedElement element,
                                                                          @NotNull Class<A> annotation) {
-        return klass.isAnnotationPresent(annotation) ? klass.getAnnotation(annotation) : null;
+        return element.isAnnotationPresent(annotation) ? element.getAnnotation(annotation) : null;
     }
 
-    public static <A extends Annotation> @NotNull Optional<A> getOptionalAnnotation(@NotNull Class<?> klass,
+    public static <A extends Annotation> @NotNull Optional<A> getOptionalAnnotation(@NotNull AnnotatedElement element,
                                                                                     @NotNull Class<A> annotation) {
-        return Optional.ofNullable(getAnnotationOrNull(klass, annotation));
+        return Optional.ofNullable(getAnnotationOrNull(element, annotation));
     }
 }
