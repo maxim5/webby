a3984813c02f77fceb28d09594a9f79d2b533b83
maxim5 <smartmaxim@gmail.com>
2021-12-25 16:23:54 +0100

    ContentProvider: support ByteBuf natively

--- src/main/java/io/webby/url/caller/ContentProviderFactory.java
+++ src/main/java/io/webby/url/caller/ContentProviderFactory.java
@@ -1,6 +1,7 @@
 package io.webby.url.caller;
 
 import com.google.inject.Inject;
+import io.netty.buffer.ByteBuf;
 import io.webby.netty.marshal.Marshaller;
 import io.webby.netty.marshal.MarshallerFactory;
 import io.webby.url.annotate.Marshal;
@@ -17,6 +18,9 @@ public class ContentProviderFactory {
 
     public @Nullable ContentProvider getContentProvider(@Nullable Marshal marshal, @NotNull Class<?> klass) {
         if (marshal != null) {
+            if (klass == ByteBuf.class) {  // support byte[] and ByteBuffer?
+                return (byteBuf, charset) -> byteBuf;
+            }
             Marshaller marshaller = factory.getMarshaller(marshal);
             return (byteBuf, charset) -> marshaller.withCustomCharset(charset).readByteBuf(byteBuf, klass);
         }
