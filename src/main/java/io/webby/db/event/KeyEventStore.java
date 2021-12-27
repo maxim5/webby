6ba2a54e34698e3d2857f07cad17643b7d1cc62c
maxim5 <smartmaxim@gmail.com>
2021-12-27 13:56:49 +0100

    minor clean-up

--- src/main/java/io/webby/db/event/KeyEventStore.java
+++ src/main/java/io/webby/db/event/KeyEventStore.java
@@ -10,13 +10,4 @@ public interface KeyEventStore<K, E> extends Persistable {
     @NotNull List<E> getAll(@NotNull K key);
 
     void deleteAll(@NotNull K key);
-
-    @Override
-    void flush();
-
-    @Override
-    void forceFlush();
-
-    @Override
-    void close();
 }
