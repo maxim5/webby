d24780e1f796a4994f6beb0bc7d9a95615a7f9ea
maxim5 <smartmaxim@gmail.com>
2022-01-23 11:38:41 +0100

    add convenience methods

--- webby-core/src/main/java/io/webby/auth/user/UserStore.java
+++ webby-core/src/main/java/io/webby/auth/user/UserStore.java
@@ -1,5 +1,6 @@
 package io.webby.auth.user;
 
+import io.webby.netty.errors.NotFoundException;
 import io.webby.orm.api.ForeignInt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -11,8 +12,20 @@ public interface UserStore {
 
     @Nullable UserModel getUserByIdOrNull(int userId);
 
+    default @NotNull UserModel getUserByIdOr404(int userId) {
+        UserModel user = getUserByIdOrNull(userId);
+        NotFoundException.failIf(user == null, "User not found: id=%d", userId);
+        return user;
+    }
+
     default @Nullable UserModel getUserByIdOrNull(@NotNull ForeignInt<? extends UserModel> foreignId) {
         return foreignId.hasEntity() ? foreignId.getEntity() : getUserByIdOrNull(foreignId.getIntId());
+    }
+
+    default @NotNull UserModel getUserByIdOr404(@NotNull ForeignInt<? extends UserModel> foreignId) {
+        UserModel user = getUserByIdOrNull(foreignId);
+        NotFoundException.failIf(user == null, "User not found: foreign_id=%s", foreignId);
+        return user;
     }
 
     int createUserAutoId(@NotNull UserModel user);
