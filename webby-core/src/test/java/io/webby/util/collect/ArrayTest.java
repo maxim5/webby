9bc3261277fc056e6fe58de435ecdef7bd901454
maxim5 <smartmaxim@gmail.com>
2022-02-07 21:02:15 +0100

    extract assertPrivateFieldClass() assertion

--- webby-core/src/test/java/io/webby/util/collect/ArrayTest.java
+++ webby-core/src/test/java/io/webby/util/collect/ArrayTest.java
@@ -1,98 +1,96 @@
 package io.webby.util.collect;
 
-import io.webby.util.reflect.EasyMembers;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
+import static io.webby.testing.AssertBasics.assertPrivateFieldClass;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ArrayTest {
     @Test
-    public void array_of_empty() throws Exception {
+    public void array_of_empty() {
         Array<String> array = Array.of();
         assertArray(array);
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_of_not_null_items() throws Exception {
+    public void array_of_not_null_items() {
         Array<String> array = Array.of("1", "2");
         assertArray(array, "1", "2");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_of_duplicate_items() throws Exception {
+    public void array_of_duplicate_items() {
         Array<String> array = Array.of("1", "1");
         assertArray(array, "1", "1");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_of_nullable_items() throws Exception {
+    public void array_of_nullable_items() {
         Array<String> array = Array.of("1", null, "2");
         assertArray(array, "1", null, "2");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_of_all_nullable_items() throws Exception {
+    public void array_of_all_nullable_items() {
         Array<String> array = Array.of(null, null);
         assertArray(array, null, null);
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_of_ints() throws Exception {
+    public void array_of_ints() {
         Array<Integer> array = Array.of(1, 2, 3);
         assertArray(array, 1, 2, 3);
         assertUnderlyingArrayType(array, Integer[].class);
     }
 
     @Test
-    public void array_of_mixed_types() throws Exception {
+    public void array_of_mixed_types() {
         Array<Object> array = Array.of(1, "2");
         assertArray(array, 1, "2");
         assertUnderlyingArrayType(array, Object[].class);
     }
 
     @Test
-    public void array_builder_of_add() throws Exception {
+    public void array_builder_of_add() {
         Array<String> array = Array.Builder.of("1", "2").add("3").toArray();
         assertArray(array, "1", "2", "3");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_builder_of_add_all_vararg() throws Exception {
+    public void array_builder_of_add_all_vararg() {
         Array<String> array = Array.Builder.of("1", "2").addAll("3").toArray();
         assertArray(array, "1", "2", "3");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_builder_of_add_all_list() throws Exception {
+    public void array_builder_of_add_all_list() {
         Array<String> array = Array.Builder.of("1", "2").addAll(List.of("3", "4")).toArray();
         assertArray(array, "1", "2", "3", "4");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_builder_start_empty() throws Exception {
+    public void array_builder_start_empty() {
         Array<String> array = Array.Builder.<String>of().add("3").addAll("4").toArray();
         assertArray(array, "3", "4");
         assertUnderlyingArrayType(array, String[].class);
     }
 
     @Test
-    public void array_to_builder() throws Exception {
+    public void array_to_builder() {
         Array<String> array = Array.of("1", "2").toBuilder().add("3").addAll("4", "5").toArray();
         assertArray(array, "1", "2", "3", "4", "5");
         assertUnderlyingArrayType(array, String[].class);
@@ -110,11 +108,7 @@ public class ArrayTest {
         }
     }
 
-    private static void assertUnderlyingArrayType(@NotNull Array<?> array,
-                                                  @NotNull Class<?> klass) throws IllegalAccessException {
-        Field field = EasyMembers.findField(BaseArray.class, "arr");
-        assertNotNull(field);
-        field.setAccessible(true);
-        assertEquals(klass, field.get(array).getClass());
+    private static void assertUnderlyingArrayType(@NotNull Array<?> array, @NotNull Class<?> klass) {
+        assertPrivateFieldClass(array, "arr", klass);
     }
 }
--- webby-core/src/testFixtures/java/io/webby/testing/AssertBasics.java
+++ webby-core/src/testFixtures/java/io/webby/testing/AssertBasics.java
@@ -1,9 +1,12 @@
 package io.webby.testing;
 
+import io.webby.util.base.Unchecked;
 import io.webby.util.collect.EasyMaps;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.junit.platform.commons.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -40,5 +43,25 @@ public class AssertBasics {
         for (Object key : keys) {
             assertEquals(expectedMap.get(key), map.get(key));
         }
+    }
+
+    public static <T> void assertPrivateFieldValue(@NotNull T object, @NotNull String name, @NotNull Object expected) {
+        assertEquals(expected, getPrivateFieldValue(object, name));
+    }
+
+    public static <T> void assertPrivateFieldClass(@NotNull T object, @NotNull String name, @NotNull Class<?> expected) {
+        Object value = getPrivateFieldValue(object, name);
+        assertNotNull(value);
+        assertEquals(expected, value.getClass());
+    }
+
+    public static <T> Object getPrivateFieldValue(@NotNull T object, @NotNull String name) {
+        List<Field> fields = ReflectionUtils.findFields(object.getClass(),
+                                                        field -> field.getName().equals(name),
+                                                        ReflectionUtils.HierarchyTraversalMode.BOTTOM_UP);
+        assertThat(fields).hasSize(1);
+        Field field = fields.get(0);
+        field.setAccessible(true);
+        return Unchecked.Suppliers.rethrow(() -> field.get(object)).get();
     }
 }
