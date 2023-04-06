134a9f79a27566d70f5e305d7253179493b925ec
maxim <smartmaxim@gmail.com>
2023-04-06 12:08:16 +0200

    Add missing tests

--- webby-core/src/test/java/io/webby/util/collect/ImmutableArrayListTest.java
+++ webby-core/src/test/java/io/webby/util/collect/ImmutableArrayListTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.google.common.truth.Truth.assertThat;
+import static io.webby.util.collect.ImmutableArrayList.toImmutableArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -29,14 +31,14 @@ public class ImmutableArrayListTest {
     }
 
     @Test
-    public void arrayList_copyOf_simple() {
+    public void copyOf_simple() {
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3, 4), 1, 2, 3, 4);
         assertArrayList(ImmutableArrayList.copyOf(List.of(1, 2, 3, 4)), 1, 2, 3, 4);
         assertArrayList(ImmutableArrayList.copyOf((Iterable<Integer>) List.of(1, 2, 3, 4)), 1, 2, 3, 4);
     }
 
     @Test
-    public void arrayList_subList() {
+    public void subList_simple() {
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(0, 0));
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(0, 1), 1);
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(1, 1));
@@ -45,6 +47,22 @@ public class ImmutableArrayListTest {
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(2, 3), 3);
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(3, 3));
         assertArrayList(ImmutableArrayList.copyOf(1, 2, 3).subList(0, 3), 1, 2, 3);
+    }
+
+    @Test
+    public void toImmutableArrayList_simple() {
+        assertEquals(Stream.of().collect(toImmutableArrayList()), ImmutableArrayList.of());
+        assertEquals(Stream.of(1).collect(toImmutableArrayList()), ImmutableArrayList.of(1));
+        assertEquals(Stream.of(1, 2).collect(toImmutableArrayList()), ImmutableArrayList.of(1, 2));
+        assertEquals(Stream.of(1, 2, 3).collect(toImmutableArrayList()), ImmutableArrayList.of(1, 2, 3));
+    }
+
+    @Test
+    public void toImmutableArrayList_nulls() {
+        assertEquals(Stream.of((Object) null).collect(toImmutableArrayList()), ImmutableArrayList.of(null));
+        assertEquals(Stream.of(1, null).collect(toImmutableArrayList()), ImmutableArrayList.of(1, null));
+        assertEquals(Stream.of(null, null).collect(toImmutableArrayList()), ImmutableArrayList.of(null, null));
+        assertEquals(Stream.of(null, 1, null).collect(toImmutableArrayList()), ImmutableArrayList.of(null, 1, null));
     }
 
     @SafeVarargs
