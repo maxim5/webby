0a655195263379e6e2f91696c26f1e6c3f0b7412
maxim <smartmaxim@gmail.com>
2023-04-09 11:28:49 +0200

    Extend ThrowConsumer util

--- webby-core/src/main/java/io/webby/util/func/ThrowConsumer.java
+++ webby-core/src/main/java/io/webby/util/func/ThrowConsumer.java
@@ -1,6 +1,12 @@
 package io.webby.util.func;
 
+import org.jetbrains.annotations.NotNull;
+
 @FunctionalInterface
 public interface ThrowConsumer<T, E extends Throwable> {
     void accept(T t) throws E;
+
+    default @NotNull ThrowConsumer<T, E> andThen(@NotNull ThrowConsumer<? super T, E> after) {
+        return (T t) -> { accept(t); after.accept(t); };
+    }
 }
