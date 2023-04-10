d7d6ebda892ea0d26aa425bbb1c3264defaafaef
maxim5 <smartmaxim@gmail.com>
2023-04-10 16:46:41 +0200

    Fix and add tests to QueryException

--- webby-core/src/main/java/io/webby/orm/api/QueryException.java
+++ webby-core/src/main/java/io/webby/orm/api/QueryException.java
@@ -1,12 +1,18 @@
 package io.webby.orm.api;
 
+import com.carrotsearch.hppc.IntContainer;
+import com.carrotsearch.hppc.LongContainer;
 import io.webby.orm.api.query.Args;
+import io.webby.util.hppc.EasyHppc;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import static io.webby.util.collect.EasyIterables.asList;
 
 /**
  * An exception for errors during SQL query preparation, execution or result processing.
@@ -28,13 +34,13 @@ public class QueryException extends RuntimeException {
     }
 
     public QueryException(@NotNull String message, @NotNull String query, @Nullable Object arg, @Nullable Throwable cause) {
-        this(message, query, Collections.singletonList(arg), cause);
+        this(message, query, tryToUnwrap(arg), cause);
     }
 
-    public QueryException(@NotNull String message, @NotNull String query, @NotNull List<?> args, @Nullable Throwable cause) {
+    public QueryException(@NotNull String message, @NotNull String query, @NotNull Collection<?> args, @Nullable Throwable cause) {
         super(format(message, query, args), cause);
         this.query = query;
-        this.args = args;
+        this.args = asList(args);
     }
 
     /**
@@ -51,7 +57,17 @@ public class QueryException extends RuntimeException {
         return args;
     }
 
-    private static @NotNull String format(@NotNull String message, @NotNull String query, @NotNull List<?> args) {
-        return "%s. Query:\n```\n%s\n```.\nArgs: `%s`".formatted(message, query, args);
+    private static @NotNull String format(@NotNull String message, @NotNull String query, @NotNull Collection<?> args) {
+        return "%s. Query:\n```\n%s\n```\nArgs: `%s`".formatted(message, query, args);
+    }
+
+    private static @NotNull List<?> tryToUnwrap(@Nullable Object arg) {
+        if (arg instanceof IntContainer ints) {
+            return EasyHppc.toJavaList(ints);
+        }
+        if (arg instanceof LongContainer longs) {
+            return EasyHppc.toJavaList(longs);
+        }
+        return Collections.singletonList(arg);
     }
 }
