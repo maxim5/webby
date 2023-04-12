095492e91076b031b526d7b8fbcc0efe514ab045
maxim5 <smartmaxim@gmail.com>
2023-04-12 19:37:46 +0200

    Polish testing utils

--- webby-core/src/test/java/io/webby/orm/codegen/SelectMakerTest.java
+++ webby-core/src/test/java/io/webby/orm/codegen/SelectMakerTest.java
@@ -1,5 +1,6 @@
 package io.webby.orm.codegen;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.webby.orm.api.ForeignInt;
 import io.webby.orm.arch.factory.ArchFactory;
 import io.webby.orm.arch.factory.ArchTesting;
@@ -22,16 +23,16 @@ public class SelectMakerTest {
         Map<Class<?>, TableArch> archMap = buildArch(User.class, Song.class);
         SelectMaker selectMaker = new SelectMaker(archMap.get(Song.class));
 
-        assertMaker(selectMaker,
-                    """
-                    SELECT author_id
-                    FROM song
-                    """,
-                    """
-                    SELECT song.author_id, user.user_id, user.name
-                    FROM song
-                    LEFT JOIN user ON song.author_id = user.user_id
-                    """);
+        assertThat(selectMaker)
+            .assertNoFollow("""
+                SELECT author_id
+                FROM song
+                """)
+            .assertFollowOneOrMore("""
+                SELECT song.author_id, user.user_id, user.name
+                FROM song
+                LEFT JOIN user ON song.author_id = user.user_id
+                """);
     }
 
     @Test
@@ -43,22 +44,22 @@ public class SelectMakerTest {
         Map<Class<?>, TableArch> archMap = buildArch(User.class, Song.class, Single.class);
         SelectMaker selectMaker = new SelectMaker(archMap.get(Single.class));
 
-        assertMaker(selectMaker,
-                    """
-                    SELECT hit_song_id
-                    FROM single
-                    """,
-                    """
-                    SELECT single.hit_song_id, song.song_id, song.author_id
-                    FROM single
-                    LEFT JOIN song ON single.hit_song_id = song.song_id
-                    """,
-                    """
-                    SELECT single.hit_song_id, song.song_id, song.author_id, user.user_id, user.name
-                    FROM single
-                    LEFT JOIN song ON single.hit_song_id = song.song_id
-                    LEFT JOIN user ON song.author_id = user.user_id
-                    """);
+        assertThat(selectMaker)
+            .assertNoFollow("""
+                SELECT hit_song_id
+                FROM single
+                """)
+            .assertFollowOneLevel("""
+                SELECT single.hit_song_id, song.song_id, song.author_id
+                FROM single
+                LEFT JOIN song ON single.hit_song_id = song.song_id
+                """)
+            .assertFollowAll("""
+                SELECT single.hit_song_id, song.song_id, song.author_id, user.user_id, user.name
+                FROM single
+                LEFT JOIN song ON single.hit_song_id = song.song_id
+                LEFT JOIN user ON song.author_id = user.user_id
+                """);
     }
 
     private @NotNull Map<Class<?>, TableArch> buildArch(@NotNull Class<?> @NotNull ...  models) {
@@ -67,22 +68,29 @@ public class SelectMakerTest {
         return runContext.tables().getAllTables();
     }
 
-    private static void assertMaker(@NotNull SelectMaker selectMaker, @NotNull String expected) {
-        assertMaker(selectMaker, expected, expected, expected);
+    private static @NotNull SelectMakerSubject assertThat(@NotNull SelectMaker selectMaker) {
+        return new SelectMakerSubject(selectMaker);
     }
 
-    private static void assertMaker(@NotNull SelectMaker selectMaker,
-                                    @NotNull String expectedNoFollow,
-                                    @NotNull String expectedOneLevel) {
-        assertMaker(selectMaker, expectedNoFollow, expectedOneLevel, expectedOneLevel);
-    }
+    @CanIgnoreReturnValue
+    private record SelectMakerSubject(@NotNull SelectMaker selectMaker) {
+        public @NotNull SelectMakerSubject assertNoFollow(@NotNull String expected) {
+            assertEquals(expected.strip(), selectMaker.make(NO_FOLLOW).join().strip());
+            return this;
+        }
 
-    private static void assertMaker(@NotNull SelectMaker selectMaker,
-                                    @NotNull String expectedNoFollow,
-                                    @NotNull String expectedOneLevel,
-                                    @NotNull String expectedFollowAll) {
-        assertEquals(expectedNoFollow.strip(), selectMaker.make(NO_FOLLOW).join().strip());
-        assertEquals(expectedOneLevel.strip(), selectMaker.make(FOLLOW_ONE_LEVEL).join().strip());
-        assertEquals(expectedFollowAll.strip(), selectMaker.make(FOLLOW_ALL).join().strip());
+        public @NotNull SelectMakerSubject assertFollowOneLevel(@NotNull String expected) {
+            assertEquals(expected.strip(), selectMaker.make(FOLLOW_ONE_LEVEL).join().strip());
+            return this;
+        }
+
+        public @NotNull SelectMakerSubject assertFollowAll(@NotNull String expected) {
+            assertEquals(expected.strip(), selectMaker.make(FOLLOW_ALL).join().strip());
+            return this;
+        }
+
+        public @NotNull SelectMakerSubject assertFollowOneOrMore(@NotNull String expected) {
+            return assertFollowOneLevel(expected).assertFollowAll(expected);
+        }
     }
 }
