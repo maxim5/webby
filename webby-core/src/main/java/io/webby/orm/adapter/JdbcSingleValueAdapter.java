08b75542366e4d3d3b433f4cfe3eb8bd1723abb1
maxim <smartmaxim@gmail.com>
2023-04-13 17:04:44 +0200

    Add javadoc to the Adapter interfaces.

--- webby-core/src/main/java/io/webby/orm/adapter/JdbcSingleValueAdapter.java
+++ webby-core/src/main/java/io/webby/orm/adapter/JdbcSingleValueAdapter.java
@@ -2,7 +2,18 @@ package io.webby.orm.adapter;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An adapter which requires a single JDBC value to represent the entity.
+ *
+ * @param <E> the entity type
+ */
 public interface JdbcSingleValueAdapter<E> extends JdbcArrayAdapter<E> {
+    /**
+     * Returns the JDBC value corresponding to the {@code instance}.
+     * <p>
+     * The implementation may assume that {@code instance} is not-null if the adapter is only being used within
+     * {@code io.webby.orm.api.BaseTable} implementation. Otherwise, the implementation may handle the null as well.
+     */
     Object toValueObject(E instance);
 
     @Override
