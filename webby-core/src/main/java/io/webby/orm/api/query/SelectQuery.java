4b881cf6af8c2285761c32016d81b9abb9433a32
maxim5 <smartmaxim@gmail.com>
2023-04-22 16:34:00 +0200

    Add SelectQuery.intern()

--- webby-core/src/main/java/io/webby/orm/api/query/SelectQuery.java
+++ webby-core/src/main/java/io/webby/orm/api/query/SelectQuery.java
@@ -1,5 +1,11 @@
 package io.webby.orm.api.query;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface SelectQuery extends Representable, HasArgs {
     int columnsNumber();
+
+    default @NotNull SelectQuery intern() {
+        return HardcodedSelectQuery.of(repr(), args());
+    }
 }
