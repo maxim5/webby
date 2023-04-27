923247b484333fb95518657b6bd6240696649eb9
maxim <smartmaxim@gmail.com>
2023-04-27 16:49:16 +0200

    Support SelectJoin query

--- webby-core/src/main/java/io/webby/orm/api/query/FullColumn.java
+++ webby-core/src/main/java/io/webby/orm/api/query/FullColumn.java
@@ -6,10 +6,12 @@ import org.jetbrains.annotations.NotNull;
 @Immutable
 public class FullColumn extends Unit implements Column {
     private final Column column;
+    private final String table;
 
     public FullColumn(@NotNull Column column, @NotNull String table) {
         super("%s.%s".formatted(table, column.name()));
         this.column = column;
+        this.table = table;
     }
 
     @Override
@@ -20,5 +22,9 @@ public class FullColumn extends Unit implements Column {
     @Override
     public @NotNull String name() {
         return column.name();
+    }
+
+    public @NotNull String table() {
+        return table;
     }
 }
