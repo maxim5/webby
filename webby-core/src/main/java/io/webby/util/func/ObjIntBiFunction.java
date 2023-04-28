b1bbc8cf9c9250d813153f5e02e6d5cbc23704b4
maxim <smartmaxim@gmail.com>
2023-04-28 15:28:33 +0200

    Add util javadocs

--- webby-core/src/main/java/io/webby/util/func/ObjIntBiFunction.java
+++ webby-core/src/main/java/io/webby/util/func/ObjIntBiFunction.java
@@ -2,8 +2,26 @@ package io.webby.util.func;
 
 import java.util.function.BiFunction;
 
+/**
+ * Represents a function that accepts two arguments and produces a result.
+ * This is the specialization of {@link BiFunction}, one of the arguments for which is an <code>int</code>.
+ *
+ * @param <T> the type of one of the arguments to the function
+ * @param <R> the type of the result of the function
+ *
+ * @see java.util.function.Function
+ * @see java.util.function.BiFunction
+ * @see java.util.function.IntFunction
+ */
 @FunctionalInterface
 public interface ObjIntBiFunction<T, R> extends BiFunction<T, Integer, R> {
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t the first function argument
+     * @param value the second function argument
+     * @return the function result
+     */
     R apply(T t, int value);
 
     @Override
--- webby-core/src/main/java/io/webby/util/func/ThrowLongSupplier.java
+++ webby-core/src/main/java/io/webby/util/func/ThrowLongSupplier.java
@@ -1,11 +1,25 @@
 package io.webby.util.func;
 
+/**
+ * Represents a supplier of {@code long}-valued results, potentially throwing a {@link Throwable}.
+ * <p>
+ * There is no requirement that a new or distinct result be returned each
+ * time the supplier is invoked.
+ * <p>
+ * The {@code ThrowLongSupplier} interface is similar to
+ * {@link java.util.function.LongSupplier}, except that a {@code ThrowLongSupplier}
+ * can throw any kind of exception, including checked exceptions.
+ *
+ * @param <E> the type of Throwable thrown
+ * @see java.util.function.Supplier
+ * @see java.util.function.LongSupplier
+ */
 @FunctionalInterface
 public interface ThrowLongSupplier<E extends Throwable> {
     /**
-     * Gets a result.
+     * Gets a result, potentially throwing an exception.
      *
      * @return a result
      */
-    int getAsInt() throws E;
+    int getAsLong() throws E;
 }
