026be1bdf99277564a86e7d69f979bb7e72964a0
maxim <smartmaxim@gmail.com>
2023-05-05 11:26:03 +0200

    Add a test for the content type

--- webby-core/src/test/java/io/webby/netty/response/ContentTypeDetectorTest.java
+++ webby-core/src/test/java/io/webby/netty/response/ContentTypeDetectorTest.java
@@ -24,4 +24,31 @@ public class ContentTypeDetectorTest {
 
         assertOneOf(detector.guessContentType(Path.of("foo.ico")), ICON_MIME_TYPES);
     }
+
+    @Test
+    public void guessContentType_audio() {
+        assertEquals("audio/mpeg", detector.guessContentType(Path.of("foo.mp3")));
+        assertEquals("audio/mpeg", detector.guessContentType(Path.of("foo.m3a")));
+    }
+
+    @Test
+    public void guessContentType_video() {
+        assertEquals("video/mpeg", detector.guessContentType(Path.of("foo.mpg")));
+        assertEquals("video/mpeg", detector.guessContentType(Path.of("foo.mpeg")));
+        assertEquals("video/webm", detector.guessContentType(Path.of("foo.webm")));
+        assertEquals("video/x-flv", detector.guessContentType(Path.of("foo.flv")));
+    }
+
+    @Test
+    public void guessContentType_exclusive_simplemagic() {
+        assertEquals("audio/midi", detector.guessContentType(Path.of("foo.rmi")));
+        assertEquals("image/vnd.adobe.photoshop", detector.guessContentType(Path.of("foo.psd")));
+    }
+
+    @Test
+    public void guessContentType_exclusive_mimeutil() {
+        assertEquals("application/x-iphone", detector.guessContentType(Path.of("foo.iii")));
+        assertEquals("application/x-cocoa", detector.guessContentType(Path.of("foo.cco")));
+        assertEquals("application/x-httpd-imap", detector.guessContentType(Path.of("foo.imap")));
+    }
 }
