79723514830c391de8b36021a84fb49799397943
maxim <smartmaxim@gmail.com>
2023-05-19 15:31:34 +0200

    Add more interceptor tests

---
+++ webby-core/src/test/java/io/webby/auth/AuthInterceptorTest.java
@@ -0,0 +1,52 @@
+package io.webby.auth;
+
+import com.google.inject.Injector;
+import io.webby.auth.user.DefaultUser;
+import io.webby.auth.user.UserAccess;
+import io.webby.auth.user.UserModel;
+import io.webby.auth.user.UserStore;
+import io.webby.netty.errors.NotFoundException;
+import io.webby.netty.request.DefaultHttpRequestEx;
+import io.webby.testing.FakeEndpoints;
+import io.webby.testing.HttpRequestBuilder;
+import io.webby.testing.SessionBuilder;
+import io.webby.testing.Testing;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AuthInterceptorTest {
+    private final Injector injector = Testing.testStartup();
+    private final AuthInterceptor interceptor = injector.getInstance(AuthInterceptor.class);
+    private final UserStore userStore = injector.getInstance(UserStore.class);
+
+    @Test
+    public void lifecycle_enter_exit_session_unauthenticated() {
+        DefaultHttpRequestEx request = HttpRequestBuilder.get("/foo").ex();
+        request.setSession(SessionBuilder.ofId(555).build());
+
+        interceptor.enter(request, FakeEndpoints.fakeEndpoint());
+        assertThat(request.isAuthenticated()).isFalse();
+        assertThat(request.<UserModel>user()).isNull();
+    }
+
+    @Test
+    public void lifecycle_enter_exit_session_authenticated() {
+        DefaultHttpRequestEx request = HttpRequestBuilder.get("/foo").ex();
+        UserModel user = userStore.createUserAutoId(DefaultUser.newUserData(UserAccess.Simple));
+        request.setSession(SessionBuilder.ofId(555).withUser(user).build());
+
+        interceptor.enter(request, FakeEndpoints.fakeEndpoint());
+        assertThat(request.isAuthenticated()).isTrue();
+        assertThat(request.<UserModel>user()).isSameInstanceAs(user);
+    }
+
+    @Test
+    public void lifecycle_enter_exit_session_with_non_existing_user() {
+        DefaultHttpRequestEx request = HttpRequestBuilder.get("/foo").ex();
+        request.setSession(SessionBuilder.ofId(555).withUserId(999).build());
+
+        assertThrows(NotFoundException.class, () -> interceptor.enter(request, FakeEndpoints.fakeEndpoint()));
+    }
+}
--- webby-core/src/test/java/io/webby/auth/session/SessionInterceptorTest.java
+++ webby-core/src/test/java/io/webby/auth/session/SessionInterceptorTest.java
@@ -1,15 +1,40 @@
 package io.webby.auth.session;
 
+import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.webby.netty.HttpConst;
+import io.webby.netty.intercept.attr.Attributes;
+import io.webby.netty.request.DefaultHttpRequestEx;
+import io.webby.testing.HttpRequestBuilder;
 import io.webby.testing.SessionBuilder;
+import io.webby.testing.Testing;
 import org.junit.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SessionInterceptorTest {
+    private final SessionInterceptor interceptor = Testing.testStartup().getInstance(SessionInterceptor.class);
+
+    @Test
+    public void lifecycle_enter_exit_without_cookies() {
+        DefaultHttpRequestEx request = HttpRequestBuilder.get("/foo").ex();
+        DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        assertThat(request.attr(Attributes.Session)).isNull();
+
+        interceptor.enter(request);
+        assertThat(request.session().sessionId()).isGreaterThan(0);
+        assertThat(request.session().isAuthenticated()).isFalse();
+
+        interceptor.exit(request, response);
+        assertThat(response.headers().get(HttpConst.SET_COOKIE)).isNotNull();
+    }
+
     @Test
     public void shouldRefresh_created_just_now() {
         DefaultSession session = SessionBuilder.ofId(123).createdAt(Instant.now()).build();
