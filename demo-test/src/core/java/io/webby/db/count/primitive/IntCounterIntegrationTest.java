947581a602e1baa3b8f2b6bb68b8d1a29fe96906
maxim <smartmaxim@gmail.com>
2023-05-26 17:09:12 +0200

    Refactor IntCounterIntegrationTest and StandaloneNettyIntegrationTest assertions

--- demo-test/src/core/java/io/webby/db/count/primitive/IntCounterIntegrationTest.java
+++ demo-test/src/core/java/io/webby/db/count/primitive/IntCounterIntegrationTest.java
@@ -36,7 +36,7 @@ public class IntCounterIntegrationTest {
     @EnumSource(Scenario.class)
     public void empty_state_counts(Scenario scenario) {
         setup(scenario);
-        assertCountEstimates(A, 0, B, 0, C, 0);
+        assertCounter(counter).hasCountEstimates(A, 0, B, 0, C, 0);
     }
 
     @ParameterizedTest
@@ -45,7 +45,7 @@ public class IntCounterIntegrationTest {
         setup(scenario);
         assertThat(counter.increment(A)).isEqualTo(1);
         assertThat(counter.decrement(B)).isEqualTo(-1);
-        assertCountEstimates(A, 1, B, -1, C, 0);
+        assertCounter(counter).hasCountEstimates(A, 1, B, -1, C, 0);
     }
 
     @ParameterizedTest
@@ -53,7 +53,7 @@ public class IntCounterIntegrationTest {
     public void empty_state_simple_update(Scenario scenario) {
         setup(scenario);
         assertThat(counter.update(A, 7)).isEqualTo(7);
-        assertCountEstimates(A, 7, B, 0, C, 0);
+        assertCounter(counter).hasCountEstimates(A, 7, B, 0, C, 0);
     }
 
     @ParameterizedTest
@@ -63,20 +63,26 @@ public class IntCounterIntegrationTest {
         assertThat(counter.update(A, 5)).isEqualTo(5);
         assertThat(counter.update(B, -3)).isEqualTo(-3);
         assertThat(counter.update(C, 0)).isEqualTo(0);
-        assertCountEstimates(A, 5, B, -3, C, 0);
+        assertCounter(counter).hasCountEstimates(A, 5, B, -3, C, 0);
 
         assertThat(counter.update(A, 2)).isEqualTo(7);
         assertThat(counter.update(B, 1)).isEqualTo(-2);
         assertThat(counter.update(C, -1)).isEqualTo(-1);
-        assertCountEstimates(A, 7, B, -2, C, -1);
+        assertCounter(counter).hasCountEstimates(A, 7, B, -2, C, -1);
     }
 
-    private void assertCountEstimates(int... expected) {
-        IntIntHashMap expectedMap = newIntMap(expected);
+    private static @NotNull IntCounterSubject assertCounter(@NotNull IntCounter counter) {
+        return new IntCounterSubject(counter);
+    }
 
-        assertMap(counter.estimateCounts(expectedMap.keys())).trimmed().containsExactlyTrimmed(expected);
-        for (IntIntCursor cursor : expectedMap) {
-            assertThat(counter.estimateCount(cursor.key)).isEqualTo(cursor.value);
+    private record IntCounterSubject(@NotNull IntCounter counter) {
+        public void hasCountEstimates(int... expected) {
+            IntIntHashMap expectedMap = newIntMap(expected);
+
+            assertMap(counter.estimateCounts(expectedMap.keys())).trimmed().containsExactlyTrimmed(expected);
+            for (IntIntCursor cursor : expectedMap) {
+                assertThat(counter.estimateCount(cursor.key)).isEqualTo(cursor.value);
+            }
         }
     }
 
