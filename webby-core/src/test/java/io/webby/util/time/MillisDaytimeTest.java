6fc3bcefdbe51071f6a60f17fa71c19a163da6d4
maxim <smartmaxim@gmail.com>
2023-08-15 18:03:41 +0200

    Minor

--- webby-core/src/test/java/io/webby/util/time/MillisDaytimeTest.java
+++ webby-core/src/test/java/io/webby/util/time/MillisDaytimeTest.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -13,15 +12,14 @@ public class MillisDaytimeTest {
     @Test
     public void localTime_roundtrip() {
         assertLocalTimeRoundtrip(LocalTime.of(0, 0, 0, 0));
-        assertLocalTimeRoundtrip(LocalTime.of(0, 0, 0, 999_999_999));
-        assertLocalTimeRoundtrip(LocalTime.of(11, 59, 59, 999_999_999));
-        assertLocalTimeRoundtrip(LocalTime.of(12, 30, 15, 777_777_777));
+        assertLocalTimeRoundtrip(LocalTime.of(0, 0, 0, 999_000_000));
+        assertLocalTimeRoundtrip(LocalTime.of(11, 59, 59, 999_000_000));
+        assertLocalTimeRoundtrip(LocalTime.of(12, 30, 15, 777_000_000));
         assertLocalTimeRoundtrip(LocalTime.of(18, 0, 0, 0));
-        assertLocalTimeRoundtrip(LocalTime.of(23, 59, 59, 999_999_999));
+        assertLocalTimeRoundtrip(LocalTime.of(23, 59, 59, 999_000_000));
     }
 
     private static void assertLocalTimeRoundtrip(@NotNull LocalTime localTime) {
-        localTime = localTime.truncatedTo(ChronoUnit.MILLIS);
         int timestamp = MillisDaytime.localTimeToMillis(localTime);
         LocalTime back = MillisDaytime.millisToLocalTime(timestamp);
         assertThat(back).isEqualTo(localTime);
