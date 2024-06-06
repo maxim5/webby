d309df62ef94d5bee58a5362fd20dacca2a0eca1
maxim <smartmaxim@gmail.com>
2024-06-06 17:52:25 +0300

    Add ColumnEnumMakerTest

---
+++ webby-core/src/test/java/io/webby/orm/codegen/ColumnEnumMakerTest.java
@@ -0,0 +1,45 @@
+package io.webby.orm.codegen;
+
+import io.webby.orm.arch.model.Column;
+import io.webby.orm.arch.model.JdbcType;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+
+public class ColumnEnumMakerTest {
+    @Test
+    public void make_different_column_types() {
+        List<Column> columns = List.of(
+            Column.of("a", JdbcType.Boolean),
+            Column.of("b", JdbcType.Int),
+            Column.of("c", JdbcType.Long),
+            Column.of("d", JdbcType.Short),
+            Column.of("e", JdbcType.Byte),
+            Column.of("f", JdbcType.Float),
+            Column.of("g", JdbcType.Double),
+            Column.of("h", JdbcType.String),
+            Column.of("i", JdbcType.Bytes),
+            Column.of("j", JdbcType.Date),
+            Column.of("k", JdbcType.Time),
+            Column.of("l", JdbcType.Timestamp)
+        );
+        Snippet snippet = ColumnEnumMaker.make(columns.stream().map(x -> x.prefixed("prefix")).toList());
+        assertThat(snippet.joinLines()).isEqualTo("""
+            a(TermType.BOOL)
+            b(TermType.NUMBER)
+            c(TermType.NUMBER)
+            d(TermType.NUMBER)
+            e(TermType.NUMBER)
+            f(TermType.NUMBER)
+            g(TermType.NUMBER)
+            h(TermType.STRING)
+            i(TermType.STRING)
+            j(TermType.TIME)
+            k(TermType.TIME)
+            l(TermType.TIME)\
+            """);
+    }
+}
