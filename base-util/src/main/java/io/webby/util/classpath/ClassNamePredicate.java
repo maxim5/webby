0da6e85334e5bd260272eb16a042adcdc9dcccc3
maxim <smartmaxim@gmail.com>
2024-06-13 20:14:36 +0300

    Extract classpath utils

---
+++ base-util/src/main/java/io/webby/util/classpath/ClassNamePredicate.java
@@ -0,0 +1,36 @@
+package io.webby.util.classpath;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.BiPredicate;
+
+/**
+ * Represents the predicate filter of the class info: class name and package name.
+ * Used by the {@link ClasspathScanner} to filter out classes before loading them.
+ * @see ClasspathScanner
+ */
+@FunctionalInterface
+public interface ClassNamePredicate extends BiPredicate<String, String> {
+    ClassNamePredicate ALLOW_ALL = (packageName, simpleClassName) -> true;
+
+    @Override
+    boolean test(@NotNull String packageName, @NotNull String simpleClassName);
+
+    @NotNull
+    @Override
+    default ClassNamePredicate and(@NotNull BiPredicate<? super String, ? super String> other) {
+        return (packageName, simpleClassName) -> test(packageName, simpleClassName) &&
+                                                 other.test(packageName, simpleClassName);
+    }
+
+    @Override
+    default @NotNull ClassNamePredicate negate() {
+        return (packageName, simpleClassName) -> !test(packageName, simpleClassName);
+    }
+
+    @Override
+    default @NotNull ClassNamePredicate or(@NotNull BiPredicate<? super String, ? super String> other) {
+        return (packageName, simpleClassName) -> test(packageName, simpleClassName) ||
+                                                 other.test(packageName, simpleClassName);
+    }
+}
