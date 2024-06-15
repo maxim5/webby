f84403e995dcf9ffd81e9476b4345a108096d34e
maxim <smartmaxim@gmail.com>
2024-06-16 00:56:31 +0300

    Convert `webby-routekit` module to kotlin

---
+++ webby-routekit/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+version = "0.3.0"
+
+// Include exported util sources
+tasks.compileJava.get().dependsOn(":base-util:exportSrc")
+sourceSets {
+    main.get().java.srcDirs(listOf("${project(":base-util").extra.get("exportDir")}/arrays"))
+}
+
+dependencies {
+    implementation(project(":base-util"))
+    testImplementation(testFixtures(project(":base-util")))
+
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("org.openjdk.jmh:jmh-core:1.37")
+    testImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+}
