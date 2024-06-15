15e88ce610fa13057071241efb91cc43b868fd67
maxim <smartmaxim@gmail.com>
2024-06-16 01:13:48 +0300

    Convert `settings.gradle` to kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "webby"
+
+include("base-util")
+include("orm-api")
+include("orm-generator")
+include("webby-core")
+include("webby-routekit")
+
+include("demo-model")
+include("demo-frontend")
+include("demo-test")
