1769501f1c056cd2209eaf635beba6b586ea6924
maxim <smartmaxim@gmail.com>
2024-06-15 17:40:36 +0300

    `demo-test`: use extension functions/fields to simplify the setup

--- demo-test/build.gradle.kts
+++ demo-test/build.gradle.kts
@@ -15,6 +15,20 @@ sourceSets {
     test.get().java.setSrcDirs(listOf<String>())
 }
 
+val core = sourceSets.named("core").get()
+val demo = sourceSets.named("demo").get()
+val tables = sourceSets.named("tables").get()
+val testFixtures = sourceSets.named("testFixtures").get()
+
+private fun SourceSet.implementation(dependencyNotation: Any) =
+    dependencies.add("${this.name}Implementation", dependencyNotation)
+
+private val SourceSet.implementation
+    get() = object {
+        fun extendsFrom(superConfig: Configuration) =
+            configurations["${name}Implementation"].extendsFrom(superConfig)
+    }
+
 // Useful:
 // https://leanmind.es/en/blog/test-fixtures-with-gradle/
 // https://remonsinnema.com/2016/05/09/how-to-manage-dependencies-in-a-gradle-multi-project-build/
@@ -22,24 +36,19 @@ sourceSets {
 // https://newbedev.com/multi-project-test-dependencies-with-gradle
 // https://stackoverflow.com/questions/32122363/gradle-sourceset-depends-on-another-sourceset
 dependencies {
-    sourceSets.named("core") {
-        "coreImplementation"("com.volkhart.memory:measurer:0.1.1")
-        // JMH
-        "coreImplementation"("org.openjdk.jmh:jmh-core:1.37")
-        "coreImplementation"("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+    core.implementation("com.volkhart.memory:measurer:0.1.1")
+    core.implementation("org.openjdk.jmh:jmh-core:1.37")
+    core.implementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+
+    listOf(core, demo, tables).forEach {
+        it.implementation(project(":demo-model"))
+        it.implementation(project(":demo-frontend"))
+        it.implementation(testFixtures(project))
+        it.implementation(testFixtures(project(":webby-core")))
+        it.implementation.extendsFrom(configurations.common.get())
     }
 
-    listOf("core", "demo", "tables").forEach {
-        sourceSets.named(it) {
-            "${this.name}Implementation"(project(":demo-model"))
-            "${this.name}Implementation"(project(":demo-frontend"))
-            "${this.name}Implementation"(testFixtures(project))
-            "${this.name}Implementation"(testFixtures(project(":webby-core")))
-            configurations["${this.name}Implementation"].extendsFrom(configurations.common.get())
-        }
-    }
-
-    testFixturesImplementation(testFixtures(project(":webby-core")))
-    testFixturesImplementation(project(":demo-frontend"))  // DevPaths
-    configurations["testFixturesImplementation"].extendsFrom(configurations.common.get())
+    testFixtures.implementation(testFixtures(project(":webby-core")))
+    testFixtures.implementation(project(":demo-frontend"))  // DevPaths
+    testFixtures.implementation.extendsFrom(configurations.common.get())
 }
