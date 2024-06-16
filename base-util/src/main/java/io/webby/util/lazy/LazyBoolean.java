e15c3304e8b7b834f68725289a872e31dcb44237
maxim <smartmaxim@gmail.com>
2024-06-17 01:07:53 +0300

    Optimize `LazyBoolean` implementation

--- base-util/src/main/java/io/webby/util/lazy/LazyBoolean.java
+++ base-util/src/main/java/io/webby/util/lazy/LazyBoolean.java
@@ -1,7 +1,9 @@
 package io.webby.util.lazy;
 
+import com.google.errorprone.annotations.ThreadSafe;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BooleanSupplier;
 
 /**
@@ -10,19 +12,19 @@ import java.util.function.BooleanSupplier;
  * Design note: the API is most convenient for flag cache/memoize use case, e.g.
  * when a flag should be computed once and used multiple times. With boolean an uninitialized value does not exist.
  */
+@ThreadSafe
 public class LazyBoolean {
     private final BooleanSupplier supplier;
-    private boolean computed;
-    private boolean value;
+    private final AtomicBoolean computed = new AtomicBoolean(false);
+    private volatile boolean value;
 
     public LazyBoolean(@NotNull BooleanSupplier supplier) {
         this.supplier = supplier;
     }
 
     public boolean get() {
-        if (!computed) {
+        if (computed.compareAndSet(false, true)) {
             value = supplier.getAsBoolean();
-            computed = true;
         }
         return value;
     }
