eaa5be3a8580ae9ed9e7387d4fcc48131338d505
maxim <smartmaxim@gmail.com>
2024-06-16 12:37:42 +0300

    Add `Instant64CodecTest` test

---
+++ webby-core/src/test/java/io/webby/db/codec/standard/Instant64CodecTest.java
@@ -0,0 +1,18 @@
+package io.webby.db.codec.standard;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static io.webby.db.codec.AssertCodec.assertCodec;
+
+public class Instant64CodecTest {
+    @Test
+    public void instant_roundtrip() throws Exception {
+        assertCodec(Instant64Codec.INSTANCE).roundtrip(Instant.now());
+        assertCodec(Instant64Codec.INSTANCE).roundtrip(Instant.ofEpochMilli(0L));
+        assertCodec(Instant64Codec.INSTANCE).roundtrip(Instant.ofEpochMilli(2_000_000_000_000L));
+        assertCodec(Instant64Codec.INSTANCE).roundtrip(Instant.ofEpochMilli(4_000_000_000_000L));
+        assertCodec(Instant64Codec.INSTANCE).roundtrip(Instant.parse("2510-12-08T18:01:03.533116200Z"));
+    }
+}
