d9acbade43dff13101d493a95c18e3dad3cca93b
maxim <smartmaxim@gmail.com>
2024-06-18 15:37:43 +0300

    Introduce `DoubleLong` utility (in beta)

--- base-util/src/testFixtures/java/io/webby/testing/TestingPrimitives.java
+++ base-util/src/testFixtures/java/io/webby/testing/TestingPrimitives.java
@@ -1,5 +1,10 @@
 package io.webby.testing;
 
+import com.google.common.primitives.UnsignedLong;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+
 public class TestingPrimitives {
     public static int[] ints(int... values) {
         return values;
@@ -7,5 +12,14 @@ public class TestingPrimitives {
 
     public static long[] longs(long... values) {
         return values;
+    }
+
+    public static boolean fitsIntoLong(@NotNull BigInteger bigInteger) {
+        return BigInteger.valueOf(Long.MAX_VALUE).compareTo(bigInteger) >= 0 &&
+               BigInteger.valueOf(Long.MIN_VALUE).compareTo(bigInteger) <= 0;
+    }
+
+    public static @NotNull BigInteger toBigInteger(long high, long low) {
+        return BigInteger.valueOf(high).shiftLeft(64).add(UnsignedLong.fromLongBits(low).bigIntegerValue());
     }
 }
